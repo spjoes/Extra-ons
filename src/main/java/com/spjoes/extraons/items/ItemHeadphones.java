7bd1142661c5f8e6fc5579d46e87411bd62d2560
Dbrown55 <dbrown55.gaming@gmail.com>
2017-12-30 19:03:48 +0100

    Well now you can use the Gui and a dispenser too

--- src/main/java/com/spjoes/extraons/items/ItemHeadphones.java
+++ src/main/java/com/spjoes/extraons/items/ItemHeadphones.java
@@ -1,26 +1,27 @@
 package com.spjoes.extraons.items;
 
-import java.lang.reflect.Field;
 import java.util.List;
 
-import com.mrcrayfish.device.api.app.component.Text;
-import com.mrcrayfish.device.init.DeviceBlocks;
+import com.google.common.base.Predicates;
 
-import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockDispenser;
+import net.minecraft.dispenser.IBehaviorDispenseItem;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.EnumDyeColor;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.EntitySelectors;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.Constants;
 
 public class ItemHeadphones extends ItemLinkable {
 
@@ -28,7 +29,31 @@ public class ItemHeadphones extends ItemLinkable {
 		super();
 		this.setRegistryName("headphones");
 		this.setUnlocalizedName("headphones");
+		BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(this, new IBehaviorDispenseItem() {	
+			@Override
+			public ItemStack dispense(IBlockSource source, ItemStack stack) {
+				BlockPos blockpos = source.getBlockPos().offset((EnumFacing)source.getBlockState().getValue(BlockDispenser.FACING));
+		        List<EntityLivingBase> list = source.getWorld().<EntityLivingBase>getEntitiesWithinAABB(EntityLivingBase.class, new AxisAlignedBB(blockpos), Predicates.and(EntitySelectors.NOT_SPECTATING, new EntitySelectors.ArmoredMob(stack)));
+
+		        if (list.isEmpty()) {
+		        	return ItemStack.EMPTY;
+		        } else {
+		        	EntityLivingBase entitylivingbase = list.get(0);
+		        	EntityEquipmentSlot entityequipmentslot = EntityEquipmentSlot.HEAD;
+		        	ItemStack itemstack = stack.splitStack(1);
+		        	entitylivingbase.setItemStackToSlot(entityequipmentslot, itemstack);
+
+		        	if(entitylivingbase instanceof EntityLiving) {
+		        		((EntityLiving)entitylivingbase).setDropChance(entityequipmentslot, 2.0F);
+		        	}
+
+		        	return stack;
+		        }
+			}
+		});
 	}
+	
+	//ItemArmor
 	
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
@@ -51,6 +76,11 @@ public class ItemHeadphones extends ItemLinkable {
 	@Override
 	public EntityEquipmentSlot[] getCorrectSlots() {
 		return new EntityEquipmentSlot[] {EntityEquipmentSlot.HEAD};
+	}
+	
+	@Override
+	public boolean isValidArmor(ItemStack stack, EntityEquipmentSlot armorType, Entity entity) {
+		return armorType == EntityEquipmentSlot.HEAD;
 	}
 	
 }
