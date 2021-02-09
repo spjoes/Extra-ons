20e38bfd0885a63318062f855d858d1a9ad5028b
spjoes <34802651+spjoes@users.noreply.github.com>
2021-02-09 18:51:59 -0500

    Fixed a thing

--- src/main/java/com/split/extraons/items/dyingkits/YellowDyingKit.java
+++ src/main/java/com/split/extraons/items/dyingkits/YellowDyingKit.java
@@ -1,27 +1,9 @@
 package com.split.extraons.items.dyingkits;
 
-import com.split.extraons.Main;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
-import static com.split.extraons.blocks.GamingChairBlock.COLORID;
 
 public class YellowDyingKit extends Item {
     public YellowDyingKit(Settings settings) {
         super(settings);
-    }
-
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand, BlockPos pos, BlockState blockState) {
-        if (blockState.getBlock() == Main.PLAIN_GAMING_CHAIR_BLOCK) {
-            world.setBlockState(pos, blockState.with(COLORID, 1));
-        }
-        return TypedActionResult.success(playerEntity.getStackInHand(hand));
     }
 }
